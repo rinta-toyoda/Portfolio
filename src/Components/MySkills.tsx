7375da40d27ae84a61e9de26c1d57acc2874b6c9
rinta-toyoda <rinta.toyoda02@gmail.com>
2025-03-29 17:51:38 +1100

    deleted files

--- src/Components/MySkills.tsx
+++
@@ -1,9 +0,0 @@
-import SectionWrapper from "./SectionWrapper.tsx";
-
-export default function MySkills() {
-  return (
-    <SectionWrapper title="My Skills">
-      <p>Skills</p>
-    </SectionWrapper>
-  );
-}
