42942c2e087505c40ddc94c4e4e2394fb9315f18
rinta-toyoda <rinta.toyoda02@gmail.com>
2025-03-30 11:25:04 +1100

    Fixed bubble bug

--- src/Components/Utils/Bubble.tsx
+++ src/Components/Utils/Bubble.tsx
@@ -4,7 +4,9 @@ export default function Bubble({ item }: { item: string }) {
   return (
     <motion.div
       whileHover={{ scale: 1.2, backgroundColor: "#304463" }}
-      className="h-[2rem] w-[7rem] bg-[#7D8ABC] rounded-xl text-center font-bold text-[1.25rem] text-[#FFF8DB] "
+      className={`
+        p-2 bg-[#7D8ABC] rounded-xl text-center font-bold text-[1.25rem] text-[#FFF8DB]
+      `}
     >
       {item}
     </motion.div>
