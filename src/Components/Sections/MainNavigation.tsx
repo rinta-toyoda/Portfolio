9ce1324708649acc9c5b101777b64aff033312bf
rinta-toyoda <rinta.toyoda02@gmail.com>
2025-03-30 15:30:50 +1100

    MainNavigation fixed2

--- src/Components/Sections/MainNavigation.tsx
+++ src/Components/Sections/MainNavigation.tsx
@@ -5,11 +5,9 @@ export default function MainNavigation() {
   return (
     <motion.header
       whileHover={{ backgroundColor: "rgba(255,248,219,1)" }}
-      className="group flex justify-between fixed left-0 right-0 h-20 top-[-10px] bg-opacity-30 bg-[#FFF8DB] text-[#FFF8DB] hover:text-[#7D8ABC] z-20"
+      className="group flex justify-between fixed top-0 left-0 right-0 h-20 bg-opacity-30 bg-[#FFF8DB] text-[#FFF8DB] hover:text-[#7D8ABC] z-20"
     >
-      <div className="flex-1 font-bold text-4xl ml-4 pt-6">
-        Rinta's Portfolio
-      </div>
+      <div className="font-bold text-4xl ml-4 pt-6">Rinta's Portfolio</div>
       <motion.a
         href="https://github.com/rinta-toyoda"
         target="_blank"
