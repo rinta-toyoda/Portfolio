6b5edaa215e305f0964f58a4c2a56ca8159ff646
rinta-toyoda <rinta.toyoda02@gmail.com>
2025-03-30 15:20:52 +1100

    MainNavigation fixed

--- src/Components/Sections/MainNavigation.tsx
+++ src/Components/Sections/MainNavigation.tsx
@@ -5,21 +5,17 @@ export default function MainNavigation() {
   return (
     <motion.header
       whileHover={{ backgroundColor: "rgba(255,248,219,1)" }}
-      className="group w-full h-20 top-[-10px] bg-opacity-30 bg-[#FFF8DB] text-[#FFF8DB] hover:text-[#7D8ABC] fixed"
+      className="group flex justify-between w-full h-20 top-[-10px] bg-opacity-30 bg-[#FFF8DB] text-[#FFF8DB] hover:text-[#7D8ABC] z-20 fixed"
     >
-      <div className="font-bold text-4xl ml-4 pt-6">Rinta's Portfolio</div>
+      <div className="flex-1 font-bold text-4xl ml-4 pt-6">
+        Rinta's Portfolio
+      </div>
       <motion.a
         href="https://github.com/rinta-toyoda"
         target="_blank"
-        initial={{ scale: 1, originX: 0.5, originY: 0.5 }}
-        whileHover={{ scale: 1.2, originX: 0.5, originY: 0.5 }}
-        style={{
-          width: 40,
-          height: 40,
-          display: "inline-block",
-          transformOrigin: "50% 50%",
-        }}
-        className="absolute right-5 top-6 cursor-pointer"
+        initial={{ scale: 1 }}
+        whileHover={{ scale: 1.2 }}
+        className="cursor-pointer mt-6 mr-3"
       >
         <FaGithub size={40} className="group-hover:text-black" />
       </motion.a>
