77aa356ff5dc08415964730e6ffa62ed70dc5e09
rinta-toyoda <rinta.toyoda02@gmail.com>
2025-03-30 11:47:44 +1100

    EducationData Finished 2

--- src/Components/Datas/EducationDatas.tsx
+++ src/Components/Datas/EducationDatas.tsx
@@ -1,4 +1,6 @@
-import { LuNotebookPen } from "react-icons/lu";
+import { LuBrain } from "react-icons/lu";
+import { RiRobot2Line } from "react-icons/ri";
+import { FcEngineering } from "react-icons/fc";
 
 const ICON_SIZE = 90;
 
@@ -7,7 +9,7 @@ export const EducationDatas = [
     id: 1,
     name: "Brain Science Training Program, RIKEN Center for Brain Science",
     date: "11/10/2022 – 20/06/2023",
-    icon: <LuNotebookPen size={ICON_SIZE} />,
+    icon: <LuBrain size={ICON_SIZE} color="#FFB74D" />,
     description:
       "Gained knowledge about the basics and the latest researches of Neuroscience and Neural Engineering",
     skills: ["Neuroscience"],
@@ -16,7 +18,7 @@ export const EducationDatas = [
     id: 2,
     name: "Bachelor of Engineering, Waseda University",
     date: "03/04/2020-21/03/2024",
-    icon: <LuNotebookPen size={ICON_SIZE} />,
+    icon: <FcEngineering size={ICON_SIZE} />,
     description:
       "Gained knowledge of Mathematics, Physics, Circuit Theory, Biology, Neuroscience, Machine Learning and Programming. Researched and analyzed brain signal EEG with machine learning and signal processing.",
     skills: [
@@ -38,7 +40,7 @@ export const EducationDatas = [
     id: 3,
     name: "Master of Computer Science, University of Sydney",
     date: "29/07/2024–CURRENT",
-    icon: <LuNotebookPen size={ICON_SIZE} />,
+    icon: <RiRobot2Line size={ICON_SIZE} color="#26A69A" />,
     description:
       "Currently enrolled. Studying about Artifical Intelligence and Data Science. Learnt about Database Management, Project Management, Software Engineering and Networks",
     skills: [
